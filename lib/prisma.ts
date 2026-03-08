c06ff1553686bd385b2ca98d291f9b574a8df9a9
= <vivek>
2026-03-08 20:37:33 +0530

    changed the prisma config a bit - hope it works

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    errorFormat: 'pretty',
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+
+export default db;
+
+
+
 // import { PrismaClient } from '../generated/prisma/client.js'
 // // import { PrismaClient } from '@prisma/client';
 // import { withAccelerate } from '@prisma/extension-accelerate'
@@ -11,18 +27,18 @@
 // if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 
 // export default db
+/////////////////////////
+// import { PrismaClient } from '../generated/prisma/client.js'
+// // import { PrismaClient } from '@prisma/client';
+// import { withAccelerate } from '@prisma/extension-accelerate'
 
-import { PrismaClient } from '../generated/prisma/client.js'
-// import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate'
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient
+// }
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient
-}
+// // ✅ Remove the `omit` option — not supported in Prisma 5
+// const db = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
-// ✅ Remove the `omit` option — not supported in Prisma 5
-const db = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
-export default db
+// export default db
