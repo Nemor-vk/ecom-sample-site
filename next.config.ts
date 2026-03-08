ec19d5c5b008b2e7eb3cea0f5346b0137c21c93d
= <vivek>
2026-03-08 17:03:35 +0530

    added vercel engine for runtime "rhel-openssl-3.0.x"

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['ssgafvsdeptagcagtmgw.supabase.co', 'ik.imagekit.io'], // Add your hostname here
+    domains: ["ssgafvsdeptagcagtmgw.supabase.co", "ik.imagekit.io"], // Add your hostname here
   },
-   typescript: {
+  typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
